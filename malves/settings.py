9911b79b74df8d21266c38ad7a1472bead000622
Helton Teixeira <heltonteixeiradesouza@hotmail.com>
2021-03-27 18:17:57 -0300

    config lib python decouple

--- malves/settings.py
+++ malves/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from decouple import config
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -23,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'v=g$gnj2!ux553ml6biuvl^g)-nx5ai$uirvd4e)a2#a6r%97e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
