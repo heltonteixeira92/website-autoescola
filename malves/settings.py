6dd346a8b273578781531f82845bcb4e65c1e636
Helton Teixeira <heltonteixeiradesouza@hotmail.com>
2021-03-28 19:21:06 -0300

    setado nova secret_key padrão decouple e no heroku

--- malves/settings.py
+++ malves/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v=g$gnj2!ux553ml6biuvl^g)-nx5ai$uirvd4e)a2#a6r%97e'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
