4802dc2397c644ae27e6b3df9ad5ed151f574331
Helton Teixeira <heltonteixeiradesouza@hotmail.com>
2021-04-18 17:27:54 -0300

    updating project to django 3.2

--- malves/settings.py
+++ malves/settings.py
@@ -91,6 +91,8 @@ default_db_url = 'sqlite:///' + str(BASE_DIR / 'db.sqlite3')
 
 parse_database = partial(dj_database_url.parse, conn_max_age=600)
 
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
 DATABASES = {
     'default': config('DATABASE_URL', default=default_db_url, cast=parse_database)
     }
