4fef7ab98ef4cf2e07deafc672fdfd21d8151676
Helton Teixeira <heltonteixeiradesouza@hotmail.com>
2021-04-19 15:06:34 -0300

    herança e bloco de templates

--- malves/base/tests/test_home.py
+++ malves/base/tests/test_home.py
@@ -15,7 +15,7 @@ def test_status_code(resp):
 
 
 def test_title(resp):
-    assert_contains(resp, '<title>Autoescola Malves</title>')
+    assert_contains(resp, '<title>Autoescola Malves - Home</title>')
 
 
 def test_home_link(resp):
