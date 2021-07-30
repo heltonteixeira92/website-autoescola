d0ce7e5e597f083efc132d6e3dc73a902487c5a5
Helton Teixeira <heltonteixeiradesouza@hotmail.com>
2021-07-29 22:52:06 -0300

    nav fixo e redes sociais

--- servicos/models.py
+++ servicos/models.py
@@ -1,3 +1,11 @@
 from django.db import models # noqa
 
-# Create your models here.
+
+class Servicos(models.Model):
+    nome_servico = models.CharField(max_length=30)
+    subtitulo = models.CharField(max_length=30)
+    descricao = models.TextField()
+    ativo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.titulo
