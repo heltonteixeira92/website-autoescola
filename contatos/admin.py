524a8f1f7e28e495fef16bffdb7eb3526b5a197b
Helton Teixeira <heltonteixeiradesouza@hotmail.com>
2021-08-30 19:53:57 -0300

    formulario basico para contatos

--- contatos/admin.py
+++ contatos/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin # noqa
-from .models import Horario
+from .models import Horario, Contato
 
 
 @admin.register(Horario)
 class HorarioAdmin(admin.ModelAdmin):
     list_display = ('dia', 'abre', 'fecha')
+
+
+admin.site.register(Contato)
