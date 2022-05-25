2fbd8cc310d2c125df7b04b7d779244f0034a742
Helton Teixeira de Souza <helton.souza@ongame.corp.com.br>
2022-05-25 09:07:27 -0300

    model mommy to model baker

--- dashboard/tests/test_dashboard.py
+++ dashboard/tests/test_dashboard.py
@@ -1,14 +1,22 @@
-# import pytest
-# from django.urls import reverse
-#
-# from malves.django_assertions import assert_contains
-#
-#
-# @pytest.fixture
-# def resp_dashboard(client, db):
-#     resp_dashboard = client.get(reverse('dashboard:dashboard'), follow=True)
-#     return resp_dashboard
-#
-#
-# def test_status_code(resp_dashboard):
-#     assert resp_dashboard.status_code == 200
+import pytest
+from django.urls import reverse
+
+from malves.django_assertions import assert_contains
+
+
+@pytest.fixture
+def resp_dashboard(client_com_usuario_logado, db):
+    resp_dashboard = client_com_usuario_logado.get(reverse('dashboard:dashboard'), follow=True)
+    return resp_dashboard
+
+
+def test_status_code(resp_dashboard):
+    assert resp_dashboard.status_code == 200
+
+
+def test_title_dashboard(resp_dashboard):
+    assert_contains(resp_dashboard, '<title> Ae Malves - Dashboard </title>')
+
+
+def test_logout_link(resp_dashboard):
+    assert_contains(resp_dashboard, '<p class="preview-subject mb-1">Log out</p>')
